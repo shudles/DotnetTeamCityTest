6f0e2d3f7b732e1dbdc04956906814857f6a303c
Shudles <alex.mg.england@gmail.com>
2024-07-16 20:45:40 +0930

    first commit

---
+++ UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace DotnetTeamCityTest;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
